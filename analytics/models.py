af4ff9653b28abc3ca451740e04532530aa71d6c
salim34888 <salim.dautov.1989@gmail.com>
2025-04-13 15:52:43 +0300

    f

--- analytics/models.py
+++ analytics/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from users.models import User
-# fuck ya
+
 class UserProgress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     tests_completed = models.PositiveIntegerField(default=0)
     skills_improvement = models.JSONField(default=dict)
+# fdasf saidfhjk dshfjkshfjk a
