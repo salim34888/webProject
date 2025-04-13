b9164bb3e150735c252ae754cba19b1f59922db0
salim34888 <salim.dautov.1989@gmail.com>
2025-04-13 15:43:16 +0300

    qwert

--- analytics/models.py
+++ analytics/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from users.models import User
-
+# fuck ya
 class UserProgress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
